46169e42c7df21f38a349b895647b632947a5dd9
Ravi Tella <RaviTella@users.noreply.github.com>
2023-04-11 21:30:40 -0400

    Update driver.sh

--- cosmos/sql/tools/java/ycsb/aks/recipes/read/10-thousand-rps-read/driver.sh
+++ cosmos/sql/tools/java/ycsb/aks/recipes/read/10-thousand-rps-read/driver.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# setting the cluster
 kubectl config use-context $4
 
+# create recipe configmap from file that containes workload configuration 
 RECIPE_CONFIG_MAP=benchmarking-recipe-config
 STATUS_CHECK=$(kubectl get configmap  $RECIPE_CONFIG_MAP | grep $RECIPE_CONFIG_MAP )
 if [ -n "$STATUS_CHECK" ]
@@ -10,7 +12,7 @@ then
 fi    
 kubectl create configmap $RECIPE_CONFIG_MAP --from-env-file=./recipe-env-file.properties
 
-
+# create secrets config map to store secrets and UUID 
 UUID=$(cat /proc/sys/kernel/random/uuid)
 BENCHMARKING_SECRETS=benchmarking-secrets
 STATUS_CHECK=$(kubectl get secrets  $BENCHMARKING_SECRETS | grep $BENCHMARKING_SECRETS )
@@ -20,4 +22,5 @@ then
 fi
 kubectl create secret generic $BENCHMARKING_SECRETS --from-literal=GUID=$UUID --from-literal=RESULT_STORAGE_CONNECTION_STRING=$1 --from-literal=COSMOS_URI=$2 --from-literal=COSMOS_KEY=$3
 
+# create resources
 kubectl apply -f benchmarking-deployment.yaml
