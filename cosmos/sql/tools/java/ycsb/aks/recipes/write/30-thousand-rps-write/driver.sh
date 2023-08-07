a54d91b7896112ff5194078076f1a24d1ba6839f
Ravi Tella <RaviTella@users.noreply.github.com>
2023-08-07 11:41:39 -0400

    30k recipes

--- cosmos/sql/tools/java/ycsb/aks/recipes/write/30-thousand-rps-write/driver.sh
+++ cosmos/sql/tools/java/ycsb/aks/recipes/write/30-thousand-rps-write/driver.sh
@@ -1,7 +1,63 @@
 #!/bin/bash
 
-kubectl config use-context $4
+while getopts s:u:k:c:r: flag
+do
+    case "${flag}" in
+        s) storageconnstring=${OPTARG};;
+        u) cosmosuri=${OPTARG};;
+        k) cosmosmkey=${OPTARG};;
+        c) clustername=${OPTARG};;
+        r) clusterrg=${OPTARG};;
+    esac
+done
 
+usage(){  
+ echo "Usage: ./driver.sh -s '<storage-connection>' -u '<cosmos-uri>' -k '<cosmos-key>' -c '<aks-cluster-name>' -r '<aks-cluster-resource-group>'"
+ }
+
+if [[ -z "$storageconnstring" ]]
+then
+  echo "Srorage account connection string is missing. Pass it with -s flag"
+  usage
+  exit 1
+fi
+
+if [[ -z "$cosmosuri" ]]
+then
+  echo "Cosmos uri is missing. Pass it with -u flag"
+  usage
+  exit 1
+fi
+
+if [[ -z "$cosmosmkey" ]]
+then
+  echo "Cosmos key is missing. Pass it with -k flag"
+  usage
+  exit 1
+fi
+
+if [[ -z "$clustername" ]]
+then
+  echo "AKS cluster name is missing. Pass it with -c flag"
+  usage
+  exit 1
+fi
+
+if [[ -z "$clusterrg" ]]
+then
+  echo "Resource group name of the AKS cluster is missing. Pass it with -r flag"
+  usage
+  exit 1
+fi
+
+
+# get cluster credentials
+az aks get-credentials -n $clustername -g $clusterrg
+
+# setting the cluster
+kubectl config use-context $clustername
+
+# create recipe configmap from file that containes workload configuration 
 RECIPE_CONFIG_MAP=benchmarking-recipe-config
 STATUS_CHECK=$(kubectl get configmap  $RECIPE_CONFIG_MAP | grep $RECIPE_CONFIG_MAP )
 if [ -n "$STATUS_CHECK" ]
@@ -10,7 +66,7 @@ then
 fi    
 kubectl create configmap $RECIPE_CONFIG_MAP --from-env-file=./recipe-env-file.properties
 
-
+# create secrets config map to store secrets and UUID 
 UUID=$(cat /proc/sys/kernel/random/uuid)
 BENCHMARKING_SECRETS=benchmarking-secrets
 STATUS_CHECK=$(kubectl get secrets  $BENCHMARKING_SECRETS | grep $BENCHMARKING_SECRETS )
@@ -18,6 +74,10 @@ if [ -n "$STATUS_CHECK" ]
 then
      kubectl delete secret $BENCHMARKING_SECRETS
 fi
-kubectl create secret generic $BENCHMARKING_SECRETS --from-literal=GUID=$UUID --from-literal=RESULT_STORAGE_CONNECTION_STRING=$1 --from-literal=COSMOS_URI=$2 --from-literal=COSMOS_KEY=$3
+kubectl create secret generic $BENCHMARKING_SECRETS --from-literal=GUID=$UUID --from-literal=RESULT_STORAGE_CONNECTION_STRING=$storageconnstring --from-literal=COSMOS_URI=$cosmosuri --from-literal=COSMOS_KEY=$cosmosmkey
 
-kubectl apply -f benchmarking-deployment.yaml
+# generate base deployment template 
+./generate-deploymnet-file.sh
+
+# create resources
+kubectl apply -f benchmarking-deployment-generated.yaml
