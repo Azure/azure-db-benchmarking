c8a354de81201faad33a3f1217b6467f7c7c77bf
Ravi Tella <RaviTella@users.noreply.github.com>
2023-08-14 09:35:35 -0400

    Update execute.sh

--- cosmos/scripts/execute.sh
+++ cosmos/scripts/execute.sh
@@ -5,7 +5,16 @@
 
 cloud-init status --wait
 echo "##########CUSTOM_SCRIPT_URL###########: $CUSTOM_SCRIPT_URL"
+
+if pgrep -xf "bash custom-script.sh"
+then
+    echo Exiting deployment as a workload is already executing.
+    exit 1
+esle
+    echo Starting the worklaod.
 # Running custom-script in background, arm template completion wont wait on this
 # stdout and stderr will be logged in <$HOME>/custom-script.out and <$HOME>/custom-script.err
-curl -o custom-script.sh $CUSTOM_SCRIPT_URL
-nohup bash custom-script.sh > "/home/${ADMIN_USER_NAME}/agent.out" 2> "/home/${ADMIN_USER_NAME}/agent.err" &
+    curl -o custom-script.sh $CUSTOM_SCRIPT_URL
+    nohup bash custom-script.sh > "/home/${ADMIN_USER_NAME}/agent.out" 2> "/home/${ADMIN_USER_NAME}/agent.err" &
+
+fi
