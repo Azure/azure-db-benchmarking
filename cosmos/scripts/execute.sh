5ca25a014c21deb90f202aee7278435b3a679683
Ravi Tella <RaviTella@users.noreply.github.com>
2023-08-14 10:26:31 -0400

    Update execute.sh

--- cosmos/scripts/execute.sh
+++ cosmos/scripts/execute.sh
@@ -10,11 +10,10 @@ if pgrep -xf "bash custom-script.sh"
 then
     echo Exiting deployment as a workload is already executing.
     exit 1
-esle
+else
     echo Starting the worklaod.
 # Running custom-script in background, arm template completion wont wait on this
 # stdout and stderr will be logged in <$HOME>/custom-script.out and <$HOME>/custom-script.err
     curl -o custom-script.sh $CUSTOM_SCRIPT_URL
     nohup bash custom-script.sh > "/home/${ADMIN_USER_NAME}/agent.out" 2> "/home/${ADMIN_USER_NAME}/agent.err" &
-
 fi
