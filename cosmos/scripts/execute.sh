8e2052c91215fcefed1b82c44ce74a90b99ccdd7
Ravi Tella <RaviTella@users.noreply.github.com>
2023-08-14 14:49:31 -0400

    Update execute.sh

--- cosmos/scripts/execute.sh
+++ cosmos/scripts/execute.sh
@@ -8,7 +8,7 @@ echo "##########CUSTOM_SCRIPT_URL###########: $CUSTOM_SCRIPT_URL"
 
 if pgrep -xf "bash custom-script.sh"
 then
-    echo Failing the deployment as a workload is already executing.Please try after 10 minutes.
+    echo Failing the deployment as a workload is already executing. Please try after the completion of the current workload or after 10 minutes
     exit 1
 else
     echo Starting the worklaod.
