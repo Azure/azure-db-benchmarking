6d6b935ef9727672b0bcefbca7d13d5f9eb53382
Ravi Tella <RaviTella@users.noreply.github.com>
2023-08-16 10:36:22 -0400

    Enforce execution of one workload at a time on each VM (#32)
    * Update execute.sh
    
    * Update execute.sh
    
    * Update execute.sh
    
    * Update execute.sh
    
    * Update execute.sh
    
    * Update execute.sh
    
    * Update cosmos/scripts/execute.sh
    
    Co-authored-by: Naveen Singh <simplynaveen20@gmail.com>
    
    * Update execute.sh
    
    ---------
    
    Co-authored-by: Naveen Singh <simplynaveen20@gmail.com>

--- cosmos/scripts/execute.sh
+++ cosmos/scripts/execute.sh
@@ -5,7 +5,18 @@
 
 cloud-init status --wait
 echo "##########CUSTOM_SCRIPT_URL###########: $CUSTOM_SCRIPT_URL"
+
+# check to enforce only once instance of the workload is running. 
+if pgrep -xf "bash custom-script.sh"
+then
+    echo Failing the deployment as a workload is already executing. Please wait for the current workload to finish on all clients, status can be checked in Azure storage workload metadata table.
+
+
+    exit 1
+else
+    echo Starting the worklaod.
 # Running custom-script in background, arm template completion wont wait on this
 # stdout and stderr will be logged in <$HOME>/custom-script.out and <$HOME>/custom-script.err
-curl -o custom-script.sh $CUSTOM_SCRIPT_URL
-nohup bash custom-script.sh > "/home/${ADMIN_USER_NAME}/agent.out" 2> "/home/${ADMIN_USER_NAME}/agent.err" &
+    curl -o custom-script.sh $CUSTOM_SCRIPT_URL
+    nohup bash custom-script.sh > "/home/${ADMIN_USER_NAME}/agent.out" 2> "/home/${ADMIN_USER_NAME}/agent.err" &
+fi
