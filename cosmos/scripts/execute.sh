057efd084b840573de2706d45ad46d1301a6d8e2
Darshan Patnekar <dpatnekar@microsoft.com>
2024-03-05 12:14:57 -0800

    commit

--- cosmos/scripts/execute.sh
+++ cosmos/scripts/execute.sh
@@ -5,16 +5,22 @@
 
 cloud-init status --wait
 echo "##########CUSTOM_SCRIPT_URL###########: $CUSTOM_SCRIPT_URL"
+echo "##########PROJECT_NAME###########: $PROJECT_NAME"
+
+# Regex check for $PROJECT_NAME
+if [[ ! $PROJECT_NAME =~ ^[a-zA-Z0-9]+$ ]]; then
+    echo "Invalid project name. Project name should not contain hyphens, underscores, or blank spaces."
+    exit 1
+fi
 
 # check to enforce only once instance of the workload is running. 
 if pgrep -xf "bash custom-script.sh"
 then
     echo Failing the deployment as a workload is already executing. Please wait for the current workload to finish on all clients, status can be checked in Azure storage workload metadata table.
-
-
     exit 1
 else
     echo Starting the worklaod.
+
 # Running custom-script in background, arm template completion wont wait on this
 # stdout and stderr will be logged in <$HOME>/agent.out and <$HOME>/agent.err
     curl -o custom-script.sh $CUSTOM_SCRIPT_URL
