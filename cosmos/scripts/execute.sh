3d6924c7c4d42ce8be3a5dc0aeeb6a375700afc6
Darshan Patnekar <dpatnekar@microsoft.com>
2024-03-25 14:34:12 -0700

    Addressed PR comments

--- cosmos/scripts/execute.sh
+++ cosmos/scripts/execute.sh
@@ -28,5 +28,5 @@ fi
 # Running custom-script in background, arm template completion wont wait on this
 # stdout and stderr will be logged in <$HOME>/custom-script.out and <$HOME>/custom-script.err
     curl -o custom-script.sh $CUSTOM_SCRIPT_URL
-    nohup bash custom-script.sh > "/home/${ADMIN_USER_NAME}/agent.out" 2> "/home/${ADMIN_USER_NAME}/agent.err" &
+    nohup bash custom-script.sh >> "/home/${ADMIN_USER_NAME}/agent.out" 2>> "/home/${ADMIN_USER_NAME}/agent.err" &
 fi
